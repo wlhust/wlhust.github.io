70a1acd82c3d2e3f4ac210d88dfe8de00a12c14f
wang li <894946234@qq.com>
2023-05-24 20:59:06 +0800

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,23 @@
+var photos = [
+  "pipi/1-image.jpg",
+  "pipi/2-image.jpg",
+  "pipi/3-image.jpg",
+  "pipi/4-image.jpg",
+  "pipi/5-image.jpg",
+  "pipi/6-image.jpg",
+  "pipi/7-image.jpg",
+  "pipi/8-image.jpg",
+  "pipi/9-image.jpg",
+  "pipi/10-image.jpg",
+  "pipi/11-image.jpg",
+  "pipi/12-image.jpg",
+  "pipi/13-image.jpg",
+  "pipi/14-image.jpg",
+  "pipi/15-image.jpg",
+  "pipi/16-image.jpg",
+  "pipi/17-image.jpg",
+  "pipi/18-image.jpg",
+  "pipi/19-image.jpg",
+  "pipi/20-image.jpg"
+  // 添加更多图片路径...
+];
